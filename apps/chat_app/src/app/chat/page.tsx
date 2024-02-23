b9d0542453110ed188fa2990bc5b6177ad915c94
suraj subedi <surajsubedi059@gmail.com>
2024-02-23 21:23:32 +0545

    designing chat layout

--- apps/chat_app/src/app/chat/page.tsx
+++ apps/chat_app/src/app/chat/page.tsx
@@ -1,15 +1,21 @@
-import { Button } from '@repo/ui/Button'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
+    <div className='h-screen'>
     <div>Contacts</div>
-    <div>
-      chat 
+    <div className='relative container max-h-[90%] h-full border-4 border-gray-300 rounded-3xl p-20 max-w-[55%] '>
+      <h1 className='absolute top-5 font-bold text-2xl text-gray-300  '>
+      Chat 
+      </h1>
       <div>
-        <input type="text"  />
-        <Button appName='send'>Send</Button>
+
+      </div>
+
+      <div className= 'absolute bottom-5 flex gap-x-2  items-center justify-center'>
+        <input type="text" className=' border-2 border-gray-400 w-[600px] rounded-lg h-10 bg-slate-900 text-gray-200 font-medium  outline-slate-800 px-3' />
+        <button className='px-2 py-1 bg-slate-900 border-2 border-gray-400 rounded-md '>Send</button>
+
       </div>
     </div>
     </div>
