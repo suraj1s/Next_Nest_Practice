969c84aaeff9a2ade49d5a9f45bccf20ea50b7d7
suraj subedi <surajsubedi059@gmail.com>
2024-02-26 15:26:34 +0545

    adding nestjs and typeorm db

--- apps/server1/src/dbIndex.ts
+++ apps/server1/src/dbIndex.ts
@@ -4,15 +4,15 @@ import { User } from "./entity/User"
 AppDataSource.initialize().then(async () => {
 
     console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "jhon"
-    user.lastName = "doe"
-    user.age = 25
-    user.isAdmin = false
-    user.isAuthor = true
-    await AppDataSource.manager.save(user)
-    
-    console.log("Saved a new user with id: " + user.id)
+    // const user = new User()
+    // user.firstName = "jhon11"
+    // user.lastName = "doe"
+    // user.age = 25
+    // user.isAdmin = false
+    // user.isAuthor = true
+    // await AppDataSource.manager.save(user)
+
+    // console.log("Saved a new user with id: " + user.id)
 
     console.log("Loading users from the database...")
     const users = await AppDataSource.manager.find(User)
