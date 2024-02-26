7dff32439128480a5b8669a82c2e2db2b1bbde5a
suraj subedi <surajsubedi059@gmail.com>
2024-02-26 13:13:47 +0545

    setting up typeorm

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,19 +1,20 @@
-import http from 'http';
-import SocketService from './services/socket';
+import { AppDataSource } from "./data-source"
+import { User } from "./entity/User"
 
-async function startServer() {
+AppDataSource.initialize().then(async () => {
 
-    const socketService = new SocketService();
-    const httpServer = http.createServer();
-    const PORT = process.env.PORT || 8000;
+    console.log("Inserting a new user into the database...")
+    const user = new User()
+    user.firstName = "Timber"
+    user.lastName = "Saw"
+    user.age = 25
+    await AppDataSource.manager.save(user)
+    console.log("Saved a new user with id: " + user.id)
 
-    socketService.io.attach(httpServer);
+    console.log("Loading users from the database...")
+    const users = await AppDataSource.manager.find(User)
+    console.log("Loaded users: ", users)
 
-    httpServer.listen(PORT , () => console.log(`Server is running on port ${PORT}`));
+    console.log("Here you can setup and run express / fastify / any other framework.")
 
-    socketService.initListeners();
-
-    
-    }
-
-startServer();
+}).catch(error => console.log(error))
