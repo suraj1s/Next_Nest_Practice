fd283bf86953c7ef7949acdde38e5d09abbf6b07
suraj subedi <surajsubedi059@gmail.com>
2024-02-26 15:50:56 +0545

    initila commit in nest js project

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/TypeOrm';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      // host: "localhost",
+      // port: 5432,
+      // username: "test",
+      // password: "test",
+      database: 'chat_project.db',
+      synchronize: true, // This will create the database if it doesn't exist and detect changes and sync with db
+      logging: true,
+      entities: [],
+      subscribers: [],
+      migrations: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(8000, () =>
+    console.log('Server is running on http://localhost:8000'),
+  );
 }
 bootstrap();
