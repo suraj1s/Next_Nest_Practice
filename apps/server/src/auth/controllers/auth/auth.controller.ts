28c15ab49c71850cce19bc90fa810c74960d469e
suraj subedi <surajsubedi059@gmail.com>
2024-02-28 20:21:03 +0545

    minor adjustments

--- apps/server/src/auth/controllers/auth/auth.controller.ts
+++ apps/server/src/auth/controllers/auth/auth.controller.ts
@@ -1,21 +1,23 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 import { AuthPayloadDto } from 'src/auth/authDto/auth.dto';
 import { AuthService } from 'src/auth/services/auth/auth.service';
-import { LocalAuthGuard } from 'src/auth/strategies/local.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  // @UseGuards(LocalAuthGuard)
   @UseGuards(AuthGuard('local'))
-  login(@Body() authPayload: AuthPayloadDto) {
+  login(@Body() authPayload: AuthPayloadDto, @Req() req: Request) {
     console.log(authPayload, 'authPayload');
     // exceptions should be handled in coltroller
     // const user = this.authService.validateUser(authPayload);
     // if(!user) throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
-    return this.authService.validateUser(authPayload);
+    return {
+      message: 'User logged in successfully',
+      auth_token: req.user,
+    };
   }
 }
