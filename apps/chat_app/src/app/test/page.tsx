71438a8ad25f157fe1f1b4a9d41e63aeb540089f
suraj subedi <surajsubedi059@gmail.com>
2024-03-29 15:31:53 +0545

    feat: open audio and video working

--- apps/chat_app/src/app/test/page.tsx
+++ apps/chat_app/src/app/test/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Page = () => {
-  const [newCameraList, setNewCameraList] = useState<any>([]);
+  // const [newCameraList, setNewCameraList] = useState<MediaDeviceInfo[]>([]);
+  const [currentStream, setCurrentStream] = useState<MediaStream>();
+  const [streamTitle, setStreamTitle] = useState<string>("No Stream Selected");
   const openMediaDevices = async (constraints: any) => {
     return await navigator.mediaDevices.getUserMedia(constraints);
   };
@@ -12,20 +14,55 @@ const Page = () => {
     const devices = await navigator.mediaDevices.enumerateDevices();
     return devices.filter((device) => device.kind === type);
   }
-  useEffect(() => {
-    getConnectedDevices("videoinput").then((videoCameras) => {
-      console.log("Cameras found:", videoCameras);
-      setNewCameraList(videoCameras);
-    });
-  }, []);
 
-  console.log(newCameraList, "newCameraList");
-
+  // listing all the connected media devices
+  // useEffect(() => {
+  //   getConnectedDevices("videoinput").then((videoCameras) => {
+  //     console.log("Cameras found:", videoCameras);
+  //     setNewCameraList(videoCameras);
+  //   });
+  // }, []);
+  // console.log(newCameraList, "newCameraList");
   // Listen for changes to media devices and update the list accordingly
   //   navigator.mediaDevices.addEventListener("devicechange", async (event) => {
   //     const newCameraList = await getConnectedDevices("video");
   //     setNewCameraList(newCameraList);
   //   });
+
+  const stremeRef = useRef<any>(null);
+
+  useEffect(() => {
+    console.log(
+      stremeRef.current,
+      "stremeRef.current",
+      currentStream,
+      "currentStream"
+    );
+    if (stremeRef.current && currentStream) {
+      console.log("inside");
+      console.log(
+        currentStream?.getVideoTracks().length,
+        "currentStream? length"
+      );
+      console.log(
+        currentStream?.getAudioTracks().length,
+        "currentStream length"
+      );
+      if (currentStream.getVideoTracks().length > 0) {
+        setStreamTitle("Video");
+        stremeRef.current.srcObject = currentStream;
+      }
+      if (
+        currentStream.getAudioTracks().length > 0 &&
+        currentStream.getVideoTracks().length === 0
+      ) {
+        setStreamTitle("Audio");
+        stremeRef.current.srcObject = currentStream;
+      }
+    }
+  }, [currentStream]);
+
+  // console.log(currentStream, "currentStream")
 
   return (
     <div className="flex flex-col gap-5 max-w-3xl mx-auto py-20">
@@ -37,6 +74,7 @@ const Page = () => {
                 video: false,
                 audio: true,
               });
+              setCurrentStream(stream);
               console.log("Got MediaStream:", stream);
             } catch (error) {
               console.error("Error accessing media devices.", error);
@@ -53,6 +91,8 @@ const Page = () => {
                 video: true,
                 audio: true,
               });
+              setCurrentStream(stream);
+
               console.log("Got MediaStream:", stream);
             } catch (error) {
               console.error("Error accessing media devices.", error);
@@ -62,15 +102,12 @@ const Page = () => {
         >
           Video{" "}
         </button>
-        <button
-          onClick={async () => {
-            
-          }}
+        {/* <button
+          onClick={async () => {}}
           className="border border-slate-300 rounded-md  px-3 py-1 hover:bg-slate-700 hover:text-blue-400 "
         >
           Close Media{" "}
         </button>
-      
         <button
           onClick={async () => {
             const videoCameras = await getConnectedDevices("videoinput");
@@ -79,12 +116,35 @@ const Page = () => {
           className="border border-slate-300 rounded-md  px-3 py-1 hover:bg-slate-700 hover:text-blue-400 "
         >
           Get Connected divices
-        </button>
+        </button> */}
       </div>
       <div>
-        <h1>active medias</h1>
+        <h1>{streamTitle}</h1>
         <div>
-          {newCameraList.map((camera: any, index: number) => {
+          <audio
+            ref={stremeRef}
+            autoPlay
+            playsInline
+            controls = {true}
+            className={`${streamTitle === "Audio" ? "block" : "hidden"}`}
+          />
+          <video
+            ref={stremeRef}
+            autoPlay
+            playsInline
+            controls = {true}
+            className={`${streamTitle === "Video" ? "block" : "hidden"}`}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
+
+{
+  /* {newCameraList.map((camera: any, index: number) => {
             return (
               <div key={index} className="flex gap-3">
                 <p>{index + 1} </p>
@@ -95,11 +155,5 @@ const Page = () => {
                 </div>
               </div>
             );
-          })}
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Page;
+          })} */
+}
